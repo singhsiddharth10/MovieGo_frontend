7276e0cb36e7889a0c2d6d8a690a59f39a86c2ea
singhsiddharth10 <sid9958434474@gmail.com>
2022-06-26 23:04:15 +0530

    auth sreen api call added

--- src/component/authscreen/AuthScreen.js
+++ src/component/authscreen/AuthScreen.js
@@ -1,48 +1,43 @@
 import { useState } from "react";
 import "./authscreenstyle.css";
-import {  useNavigate} from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 export default function AuthScreen() {
-    const navigate =  useNavigate ();
-    const [btn, setBtn] = useState(true)
-    const [btnText, setBtnText] = useState('Login')
-    const [footerText,setFooterText] = useState('Register')
-    const [userName, setUserName] = useState("");
-    const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const [btn, setBtn] = useState(true);
+  const [btnText, setBtnText] = useState("Login");
+  const [footerText, setFooterText] = useState("Register");
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
 
-
-    const handleFooterClick = () => {
-       
-        setBtn(!btn)
-        if(btn){
-            setBtnText('Register')
-            setFooterText('Login')
-        }else
-        {
-            setBtnText('Login')
-            setFooterText('Register')
-        }
-
+  const handleFooterClick = () => {
+    setBtn(!btn);
+    if (btn) {
+      setBtnText("Register");
+      setFooterText("Login");
+    } else {
+      setBtnText("Login");
+      setFooterText("Register");
     }
-    const handleBtnClick = () => {
-        if(btn){
-            const requestdata = {
-                email : userName,
-                password : password
-            }
-            axios.post('http://localhost:8080/MovieGo/CheckUser',requestdata)
-                .then(function (response) {
-                    console.log("response",response)
-
-                }).catch(
-                function (error) {
-                    console.log(error)
-                }
-            );
-        }
-       
-
+  };
+  const handleBtnClick = async () => {
+    const formdata = `email=${userName}&password=${password}`
+    if (btn) {
+      await axios({
+        method: "post",
+        url: `http://localhost:8080/MovieGo/CheckUser`,
+        data: formdata
+      })
+        .then(function (response) {
+          if(response.data === "success"){
+            navigate("/MainScreen")
+          }
+        })
+        .catch(function (response) {
+          console.log(response);
+        });
     }
+  };
   return (
     <div className="parentContainer">
       <div className="childContainer">
@@ -65,14 +60,14 @@ export default function AuthScreen() {
               type="password"
               onChange={(e) => setPassword(e.target.value)}
             />
-            <button
-              className="loginbtn" onClick={handleBtnClick}
-            >
+            <button className="loginbtn" onClick={handleBtnClick}>
               {btnText}
             </button>
             <div className="authfooter">
               <p className="footerText">New to MovieGo?</p>
-              <div className="Regsiter" onClick={handleFooterClick}>{footerText}</div>
+              <div className="Regsiter" onClick={handleFooterClick}>
+                {footerText}
+              </div>
             </div>
           </div>
         </div>
