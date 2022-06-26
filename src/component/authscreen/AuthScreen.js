24d3d4d96d69f0f5e441551f509794e872a6b37d
singhsiddharth10 <sid9958434474@gmail.com>
2022-06-27 01:22:30 +0530

    register user functionality added

--- src/component/authscreen/AuthScreen.js
+++ src/component/authscreen/AuthScreen.js
@@ -6,31 +6,48 @@ export default function AuthScreen() {
   const navigate = useNavigate();
   const [btn, setBtn] = useState(true);
   const [btnText, setBtnText] = useState("Login");
-  const [footerText, setFooterText] = useState("Register");
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
+  const [invalidLogin, setInvalidLogin] = useState(false);
+  const [invalidRegister, setInvalidRegister] = useState(false);
 
   const handleFooterClick = () => {
-    setBtn(!btn);
-    if (btn) {
-      setBtnText("Register");
-      setFooterText("Login");
-    } else {
-      setBtnText("Login");
-      setFooterText("Register");
-    }
+    setBtn(false);
+    setBtnText("Register");
+    
   };
   const handleBtnClick = async () => {
-    const formdata = `email=${userName}&password=${password}`
+    const formdata = `email=${userName}&password=${password}`;
     if (btn) {
       await axios({
         method: "post",
         url: `http://localhost:8080/MovieGo/CheckUser`,
-        data: formdata
+        data: formdata,
       })
         .then(function (response) {
-          if(response.data === "success"){
-            navigate("/MainScreen")
+          if (response.data === "success") {
+            setInvalidLogin(false);
+            navigate("/MainScreen");
+          } else {
+            setInvalidLogin(true);
+          }
+        })
+        .catch(function (response) {
+          console.log(response);
+        });
+    } else {
+      await axios({
+        method: "post",
+        url: `http://localhost:8080/MovieGo/RegisterUser`,
+        data: formdata,
+      })
+        .then(function (response) {
+          if (response.data === "success") {
+            setInvalidRegister(false);
+            setBtn(true)
+            setBtnText("Login");
+          } else {
+            setInvalidRegister(true);
           }
         })
         .catch(function (response) {
@@ -45,9 +62,17 @@ export default function AuthScreen() {
           <div className="authHeader">
             <p>Welcome to MovieGO</p>
           </div>
+          {btn === true ? (
+            invalidLogin === true ? (
+              <div className="ivalidCred">Invalid Credentials</div>
+            ) : null
+          ) : invalidRegister === true ? (
+            <div className="ivalidCred">User already registered</div>
+          ) : null}
           <div className="authBody">
             <input
               className="inputtextAuth"
+              s
               placeholder="Username"
               value={userName}
               type="text"
@@ -63,12 +88,14 @@ export default function AuthScreen() {
             <button className="loginbtn" onClick={handleBtnClick}>
               {btnText}
             </button>
-            <div className="authfooter">
-              <p className="footerText">New to MovieGo?</p>
-              <div className="Regsiter" onClick={handleFooterClick}>
-                {footerText}
+            {btn === true ? (
+              <div className="authfooter">
+                <p className="footerText">New to MovieGo?</p>
+                <div className="Regsiter" onClick={handleFooterClick}>
+                Register
+                </div>
               </div>
-            </div>
+            ) : null}
           </div>
         </div>
       </div>
